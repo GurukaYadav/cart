9caf8407568fe4937c693b7ed5a485fdd7e692d4
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-18 21:10:50 +0530

    updating cart

---
+++ ami.tf
@@ -0,0 +1,19 @@
+module "immutable-app-ami" {
+  source = "github.com/GurukaYadav/immutable-app-ami.git"
+  COMPONENT = "cart"
+  APP_VERSION = "var.APP_VERSION"
+}
+
+variable "APP_VERSION" {}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-sfiles"
+    key    = "terraform/immutable/ami/cart/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
